fd61be29691202234f8059193c594a8aba4fc893
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-02-02 13:24:16 -0500

    qweqw

--- src/main/java/edu/iu/c322/test1/Test1Application.java
+++ src/main/java/edu/iu/c322/test1/Test1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("edu.iu.c322.test1.repository")
+
 public class Test1Application {
 
 	public static void main(String[] args) {
